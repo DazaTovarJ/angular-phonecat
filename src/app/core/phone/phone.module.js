fe543524fdeb6f643f48f121fb3279549a1e5e65
Jaime Daza <jaime.daza@hypersoft.co>
2023-07-06 16:51:00 -0500

    refactor(phone): Export phone service

--- src/app/core/phone/phone.module.js
+++ src/app/core/phone/phone.module.js
@@ -1,4 +1,7 @@
 import angular from "angular";
+import { Phone } from "./phone.service";
 
 // Define the `core.phone` module
-export const phoneModule = angular.module("core.phone", ["ngResource"]);
+export const phoneModule = angular
+  .module("core.phone", ["ngResource"])
+  .factory("Phone", Phone);
--- src/app/core/phone/phone.service.js
+++ src/app/core/phone/phone.service.js
@@ -1,15 +1,14 @@
-'use strict';
-
-angular.
-  module('core.phone').
-  factory('Phone', ['$resource',
-    function($resource) {
-      return $resource('phones/:phoneId.json', {}, {
-        query: {
-          method: 'GET',
-          params: {phoneId: 'phones'},
-          isArray: true
-        }
-      });
+Phone.$inject = ["$resource"];
+export function Phone($resource) {
+  return $resource(
+    "phones/:phoneId.json",
+    {},
+    {
+      query: {
+        method: "GET",
+        params: { phoneId: "phones" },
+        isArray: true,
+      },
     }
-  ]);
+  );
+}
