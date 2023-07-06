3b171e701594cd1dacac48b37afbd36bd08084b0
Jaime Daza <jaime.daza@hypersoft.co>
2023-07-05 19:29:01 -0500

    refactor(app): Switch to manual bootstrapping

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "jquery";
+import angular from "angular";
+import "angular-animate";
+import "angular-resource";
+import "angular-route";
+
+angular.bootstrap(document.documentElement, ["phonecatApp"]);
