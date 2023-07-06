fbf872217eec6670baa7a09a310ae1026ef87995
Jaime Daza <jaime.daza@hypersoft.co>
2023-07-06 17:02:50 -0500

    refactor(phone-detail): Rewrite phone detail component as ES6 module

--- src/app/phone-detail/phone-detail.module.js
+++ src/app/phone-detail/phone-detail.module.js
@@ -1,7 +1,8 @@
 import angular from "angular";
+import { phoneModule } from "../core/phone/phone.module";
+import { phoneDetailComponent } from "./phone-detail.component";
 
 // Define the `phoneDetail` module
-export const phoneDetailModule = angular.module("phoneDetail", [
-  "ngRoute",
-  "core.phone",
-]);
+export const phoneDetailModule = angular
+  .module("phoneDetail", ["ngRoute", phoneModule.name])
+  .component("phoneDetail", phoneDetailComponent);
