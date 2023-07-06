d6c61b54b46a3807d5768b2f1a8605687db70a53
Jaime Daza <jaime.daza@hypersoft.co>
2023-07-06 16:51:40 -0500

    refactor(phone-list): ES6 Import phone module

--- src/app/phone-list/phone-list.module.js
+++ src/app/phone-list/phone-list.module.js
@@ -1,4 +1,8 @@
 import angular from "angular";
+import { phoneListComponent } from "./phone-list.component";
+import { phoneModule } from "../core/phone/phone.module";
 
 // Define the `phoneList` module
-export const phoneListModule = angular.module("phoneList", ["core.phone"]);
+export const phoneListModule = angular
+  .module("phoneList", [phoneModule.name])
+  .component("phoneList", phoneListComponent);
