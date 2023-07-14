27c254388bdd44de800fbf88bc5484353b744394
Jaime Daza <jaime.daza@hypersoft.co>
2023-07-14 16:58:00 -0500

    test(phone-list): Convert phone list component test to TypeScript

---
+++ src/app/phone-list/phone-list.component.spec.ts
@@ -0,0 +1,42 @@
+import * as angular from 'angular';
+import { PhoneListController } from './phone-list.component';
+
+describe('phoneList', function () {
+  // Load the module that contains the `phoneList` component before each test
+  beforeEach(angular.mock.module('phoneList'));
+
+  // Test the controller
+  describe('PhoneListController', function () {
+    let $httpBackend: angular.IHttpBackendService, ctrl: PhoneListController;
+
+    beforeEach(
+      angular.mock.inject(function (
+        $componentController: angular.IComponentControllerService,
+        _$httpBackend_: angular.IHttpBackendService
+      ) {
+        $httpBackend = _$httpBackend_;
+        $httpBackend
+          .expectGET('phones/phones.json')
+          .respond([{ name: 'Nexus S' }, { name: 'Motorola DROID' }]);
+
+        ctrl = $componentController('phoneList', {});
+      })
+    );
+
+    it('should create a `phones` property with 2 phones fetched with `$http`', function () {
+      jasmine.addCustomEqualityTester(angular.equals);
+
+      expect(ctrl.phones).toEqual([]);
+
+      $httpBackend.flush();
+      expect(ctrl.phones).toEqual([
+        { name: 'Nexus S' },
+        { name: 'Motorola DROID' },
+      ]);
+    });
+
+    it('should set a default value for the `orderProp` property', function () {
+      expect(ctrl.orderProp).toBe('age');
+    });
+  });
+});
